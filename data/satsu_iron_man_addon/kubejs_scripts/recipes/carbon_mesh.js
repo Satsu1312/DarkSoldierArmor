f53ec7ce217a20b24ed48f0e9ef4008cdf734aa7
Satsu1312 <minion1312@hotmail.com>
2026-02-23 11:45:44 -0600

    Recipe

---
+++ data/satsu_iron_man_addon/kubejs_scripts/recipes/carbon_mesh.js
@@ -0,0 +1,22 @@
+// Smelting recipe template
+
+ServerEvents.recipes((event) => {
+  event
+    .smelting(
+      // output item
+      "satsu_iron_man_addon:nanopolymer_d_52",
+      "minecraft:coal_block" // input item
+    )
+    .xp(1.0) // optional: set XP
+    .cookingTime(250); // optional: cook time in ticks (200 = 10 seconds)
+});
+ServerEvents.recipes((event) => {
+  event
+    event.blasting(
+      // output item
+      "satsu_iron_man_addon:nanopolymer_d_52",
+      "minecraft:coal_block" // input item
+    )
+    .xp(1.0) // optional: set XP
+    .cookingTime(200); // optional: cook time in ticks (200 = 10 seconds)
+});
