ff517b37c8b4f039d0e22f8a3a0cc598391c6ce4
Satsu1312 <minion1312@hotmail.com>
2026-03-24 19:04:28 -0600

    New propertie

---
+++ data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -0,0 +1,8 @@
+
+PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_addon_dark_tech_falcon_wings", "string", "off");
+  }
+});
