a3eb46b2f1617e86457e178884f26931a7c33fbb
Satsu1312 <minion1312@hotmail.com>
2026-05-02 13:57:44 -0600

    Added sound

--- addon/satsu_iron_man_addon/kubejs_scripts/sounds.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/sounds.js
@@ -1,4 +1,5 @@
 
 StartupEvents.registry("sound_event", (event) => {
   event.create("satsu_iron_man_addon:entity.armor.round_shield");
+  event.create("satsu_iron_man_addon:entity.armor.ultra_beam");
 });
